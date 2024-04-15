352b902cc43363f0c03cb5f23a952b4201991079
BrianSechelo <sechbrian123@gmail.com>
2024-04-15 15:42:43 -0700

    set up MySQL test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,16 @@
+-- Creates a MySQL server with:
+-- Database hbnb_test_db.
+-- User hbnb_test with password hbnb_test_pwd in localhost
+-- Grants all privilleges for hbnb_test on hbnb_test_db.
+-- Grants SELECT privilleges for hbnb_test_db on perfomance.
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS ' hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON  hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+FLUSH PRIVILEGES;
+
